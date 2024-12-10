f538d24be8ecfa16c634e1f11aaff37e743e0cb8
Enda Figueroa <efigueroa@Zipcoders-MacBook-Pro-2.local>
2024-12-10 00:46:48 -0500

    w

--- ruby.rb
+++ ruby.rb
@@ -10,7 +10,7 @@
     # Prints upper bar
   puts left_margin + '*' * 153
     # Idea slide height is 15. This calculates the needed empty space above and below the text
-  room = 15 - arr.length
+  room = 15 - arr.length+1
   extra_room = room / 2
     # Ruby auto rounds up. This makes it round down.
   if extra_room % 2 > 0
@@ -81,29 +81,45 @@ end
 # Every slide is an array of strings. One string is one line of text.
 
 s1_intro = ["~~ Ruby is an Object Oriented Programming language.","That means it abides by the four pillars:",
-            "Abstraction", "Encapsulation", "Inheritance", "Polymorphism"]
-s2_abs = ["~~ ABSTRACTION",
-          "Ruby allows programmers to abstract real-world objects and data into classes and variables",
-          "The programmer does not need to declare the data type when creating a variable.",
-          "Casting is still needed at times and can be done with .to_ and then a letter for the data type",
-          "Classes can be used to create objects with built in attributes and methods.",
-          "A new class is made by object = object.new",
+            "Abstraction", "Encapsulation", "Inheritance", "Polymorphism",
+            " ",
+            "I'm going to show you examples of these four pillars as well as other syntax that I thought was interesting."]
+s2_abs = ["~~ ABSTRACTION ",
+          "Through the use of puts, and print, the user only sees what is needed and interacts with the program through the gets keyword.",
+          "Right now, this program finds the length of an array of strings, subtracts that length from the idea slide height,",
+          "then divides that number in half to know how many empty lines to print above and below the slide text.",
+          "It then checks to see if a string is tagged to be centered and then calculates the spaces needed before and after.",
+          "Then, it finds the amount of spaces needed before and after each line and prints the three parts in order.",
+          "Last, it prints the other half of the empty rows and then the bottom star bar. There is also a function to pause the output",
+          "And wait for an enter to show the next slide, and then clears the screen by displaying a number of enters.",
           " ",
-          "Through the use of puts, and print, the user only sees what is needed and interacts with the user through the gets keyword.",
-          "Right now, this program is taking each line and finds the space needed before, after, to the left, and right.",
-          "Showing only a slide with aligned text and a relatively uniform size."]
-s3_encap = ["~~ ENCAPSULATION",
-            "Ruby protects and hides data through the use of instance variable, attribute accessors, readers, and writers.",
+          "The result is a slide program that displays up to ten lines per slide in a relatively uniform size.",
+          "All those calculations are abstracted and hidden from the end user."]
+s3_encap = ["~~ ENCAPSULATION ",
+            "Ruby protects and hides data through the use of instance variables, attribute accessors, readers, and writers.",
             "These look like @ to denote a class' instance variable, and then attr_accessor, attr_reader, and attr_writer.",
             "These make it so that we can set or get the instance variable elsewhere. The attr_accessor is both getter and setter.",
             "You can designate what instance variables you want to be read-only,write-only, or both"]
-s4_inher = ["~~ INHERITANCE",
+s4_inher = ["~~ INHERITANCE ",
             "The use of classes and subclasses is an important aspect to Ruby.",
             "A subclass is denoted with the < symbol. Subclasses inherit all attributes and methods of its parent class.",
             "An attribute that belongs to the whole class is denoted with @@ and is often only used for instance counting since",
             "any instance of the class and/or its subclasses can alter it on accident. Instead, class instance variables are preferred",
-            "because it narrows the probability of unintended bugs."]
-s5_poly = ["~~ POLYMORPHISM"]
+            "because it narrows the probability of unintended bugs.",
+            " ",
+            "Ruby also has the ability to have a subclass' method perform the super class method, but then add to it with the use",
+            "of the super keyword."]
+s5_poly = ["~~ POLYMORPHISM  ",
+           "Polymorphism is the ability to use the same method and return different results depending on the input.",
+           "Ruby accomplishes this through the ability to change a method's behavior due to inheritance, as well as",
+           "the ability to use duck-typing where the name of something is less important than the method being used.",
+           "Ruby was designed to favor function over construction which is convenient, but can cause bugs and sometimes",
+           "be hard to debug."]
+s6_conc = ["~~ CONCLUSION ",
+           "Ruby is a fun, flexible programming language with a robust support community that continues to use and celebrate",
+           "the language decades after its launch. It is intuitive and flexible, promoting common language approach to development.",
+           " ",
+           "Personally, I found it very easy to pick up and I look forward to using it in the future."]
 ############## SLIDE SHOW ###############
 empty_screen
 box(s1_intro)
@@ -115,4 +131,6 @@ click
 box(s4_inher)
 click
 box(s5_poly)
+click
+box(s6_conc)
 click
